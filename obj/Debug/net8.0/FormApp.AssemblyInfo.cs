4d6cfc262f57cd00bec65a03db4381c325287f94
Abu Sayed <sayed.saikat25@gmail.com>
2025-02-09 16:13:40 +0600

    Added toogle button for dark, light theme

--- obj/Debug/net8.0/FormApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/FormApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db23ea0435867c4dd991653abe41651a18af5971")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8fec92e45a48ea4c42f3616e98c44752b9023e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
